d722f3803819ef70b31a0c1d9ba032a91ec0d28c
Asserio <asserio16@hotmail.com>
2016-08-05 18:10:33 +0100

    Update EvolvedZed.lua

--- EvolvedZed.lua
+++ EvolvedZed.lua
@@ -14,6 +14,7 @@ Zed.Combo:Boolean("R","Use R",true)
 --Harass
 Zed:SubMenu("Harass","Harass")
 Zed.Harass:Boolean("Q","Use Q",true)
+Zed.Harass:Boolean("W","Use W",true)
 Zed.Harass:Boolean("E","Use E",true)
 --Drawings
 Zed:SubMenu("Draw","Drawings")
@@ -29,27 +30,42 @@ Zed.Misc.Emode:Boolean("ME","Manual E",false)
 PrintChat("Evolved Zed is Loaded")
 PrintChat("Have fun senpai!")
 
-deathMark = nil
 
+deathMark = nil
+local Wtime = 0
+local objects = {}
+local stackeddamage = 0
+local savestacked = 0
 OnTick(function(myHero)
 local unit = GetCurrentTarget()
-DrawDamage(unit)
 if Zed.Misc.Emode.AE:Value() then
 	useE(unit)
 end
 if IOW:Mode() == "Combo" then
-	if Zed.Misc.Emode.ME:Value() and Zed.Combo.E:Value() then
+ 	if Zed.Misc.Emode.ME:Value() and Zed.Combo.E:Value() then
 		useE(unit)
 	end
-	gapClose(unit)
-	useQ(unit)
-	useR(unit)
+    if Zed.Combo.Q:Value() then
+	   useQ(unit)
+	end
+	if Zed.Combo.W:Value() then
+	   useW(unit)
+	   gapClose(unit)
+	end
+	if Zed.Combo.R:Value() then
+	   useR(unit)
+	end
 end
 if IOW:Mode() == "Harass" then
+
 	if Zed.Misc.Emode.ME:Value() and Zed.Harass.E:Value() then
-		useE(unit)
+	   useE(unit)
 	end
-	useQ(unit)
+	if Zed.Harass.Q:Value() then
+	   useQ(unit)
+	end
+	useW(unit)
+
 end
 end)
 
@@ -62,20 +78,49 @@ function useQ(unit)
 			CastSkillShot(_Q,Qpred.castPos)
 		end
 	end
+  for i, obj in pairs(objects) do
 	-- QW
-	if Ready(_Q) and WPos ~= nil and ValidTarget(unit, 4000) then
-		local wQPred = GetPredictionForPlayer(WPos,unit,GetMoveSpeed(unit),1700,250,GetCastRange(myHero,_Q),50,false,false)
+	if Ready(_Q) and obj ~= nil and ValidTarget(unit, 4000) then
+		local wQPred = GetPredictionForPlayer(GetOrigin(obj),unit,GetMoveSpeed(unit),1700,250,GetCastRange(myHero,_Q),50,false,false)
 			if wQPred.HitChance == 1 then
 				CastSkillShot(_Q,wQPred.PredPos.x,wQPred.PredPos.y,wQPred.PredPos.z)
 			end
 	end
 	--QR
-	if Ready(_Q) and RPos ~= nil and ValidTarget(unit, 4000) then
-		local rQPred = GetPredictionForPlayer(RPos,unit,GetMoveSpeed(unit),1700,250,GetCastRange(myHero,_Q),50,false,true)
+	if Ready(_Q) and obj ~= nil and ValidTarget(unit, 4000) then
+		local rQPred = GetPredictionForPlayer(GetOrigin(obj),unit,GetMoveSpeed(unit),1700,250,GetCastRange(myHero,_Q),50,false,true)
 		if rQPred.HitChance == 1 then
 			CastSkillShot(_Q,rQPred.PredPos.x,rQPred.PredPos.y,rQPred.PredPos.z)
 		end
 	end
+  end
+end
+
+function useW(unit)
+  if IOW:Mode() == "Harass" then
+     local time = 5000
+	 if Ready(_Q) and Ready(_W) and GotBuff(myHero,"ZedWHandler") == 0 and ValidTarget(unit, totalRange) then
+		-- GetPredictionForPlayer(StartPos,target,movespeed,speed,delay,range,width,collision(true/false),AddHitBox(true/false))
+		local Time = GetTickCount()
+		local WQPred = GetPredictionForPlayer(myHeroPos(),unit,GetMoveSpeed(unit),300,300,1450,250,false,false)
+		if (Wtime + 5000) < Time then
+			if WQPred.HitChance == 1 then
+				CastSkillShot(_W,WQPred.PredPos.x,WQPred.PredPos.y,WQPred.PredPos.z)
+				Wtime = Time
+			end
+		end
+	 end
+  end
+	for i, obj in pairs(objects) do
+		if IOW:Mode() == "Combo" then
+		if Ready(_W) and obj ~= nil and ValidTarget(unit, 4000) then
+			local WPredt = GetPredictionForPlayer(GetOrigin(obj),unit,GetMoveSpeed(unit),math.huge,250,GetCastRange(myHero,_E),0,false,false)
+			if WPredt.HitChance == 1 then
+				CastSpell(_W)
+			end
+		end
+	end
+end
 end
 
 function useE(unit)
@@ -85,38 +130,58 @@ function useE(unit)
 			CastTargetSpell(myHero,_E)
 		end
 	end
+	for i, obj in pairs(objects) do
 	-- EW
-	if Ready(_E) and WPos ~= nil and ValidTarget(unit, 4000) then
-		local wEPred = GetPredictionForPlayer(WPos,unit,GetMoveSpeed(unit),math.huge,250,GetCastRange(myHero,_E),0,false,false)
-		if wEPred.HitChance == 1 then
-			CastSpell(_E)
+		if Ready(_E) and obj ~= nil and ValidTarget(unit, 4000) then
+			local wEPred = GetPredictionForPlayer(GetOrigin(obj),unit,GetMoveSpeed(unit),math.huge,250,GetCastRange(myHero,_E),0,false,false)
+			if wEPred.HitChance == 1 then
+				CastSpell(_E)
+			end
 		end
-	end
 	--ER
-	if Ready(_E) and RPos ~= nil and ValidTarget(unit, 4000) then
-		local rEPred = GetPredictionForPlayer(RPos,unit,GetMoveSpeed(unit),math.huge,250,GetCastRange(myHero,_E),0,false,false)
-		if rEPred.HitChance == 1 then
-			CastSpell(_E)
+		if Ready(_E) and obj ~= nil and ValidTarget(unit, 4000) then
+			local rEPred = GetPredictionForPlayer(GetOrigin(obj),unit,GetMoveSpeed(unit),math.huge,250,GetCastRange(myHero,_E),0,false,false)
+			if rEPred.HitChance == 1 then
+				CastSpell(_E)
+			end
 		end
 	end
 end
 
 function useR(unit)
-local Qdmg = getdmg("Q",unit,myHero,1)
-local Edmg = getdmg("E",unit,myHero,1)
-local Rdmg = getdmg("R",unit,myHero,3)
-local TotalDamage = Qdmg + Edmg + Rdmg
+local Qdmg = getdmg("Q",unit,myHero,1,GetCastLevel(myHero,_Q))
+local Edmg = getdmg("E",unit,myHero,1,GetCastLevel(myHero,_E))
+local Rdmg = getdmg("R",unit,myHero,1,GetCastLevel(myHero,_R))
+local physical = GetBaseDamage(myHero) + GetBonusDmg(myHero)
+local magical = GetBonusAP(myHero)
+local TotalDamage = physical + magical * .1
 local enemyLife = GetCurrentHP(unit)+GetMagicShield(unit)+GetDmgShield(unit)
 
-	if ValidTarget(unit, GetCastRange(myHero, _R)) and Ready(_R) and deathMark == nil and TotalDamage > enemyLife then
+	if ValidTarget(unit, GetCastRange(myHero, _R)) and Ready(_R) and deathMark == nil and savestacked > enemyLife then
 		CastTargetSpell(unit, _R)
-		if Ready(_Q) or Ready(_E) then
-			CastTargetSpell(unit, _R)
-		end
 	end
-	if deathMark == nil and not ValidTarget(unit, GetCastRange(myHero,_E)) and Ready(_R) then
+--[[if Ready(_Q) then
+	if ValidTarget(unit, GetCastRange(myHero, _R)) and Ready(_R) and deathMark == nil and TotalDamage + Qdmg > enemyLife then
+		CastTargetSpell(unit, _R)
+	end
+end
+if Ready(_E) then
+    if ValidTarget(unit, GetCastRange(myHero, _R)) and Ready(_R) and deathMark == nil and TotalDamage + Edmg > enemyLife then
+		CastTargetSpell(unit, _R)
+	end
+end
+if Ready(_Q) and Ready(_E) then
+	if ValidTarget(unit, GetCastRange(myHero, _R)) and Ready(_R) and deathMark == nil and TotalDamage + Qdmg + Edmg > enemyLife then
+		CastTargetSpell(unit, _R)
+	end
+end]]
+-- Go back
+	if deathMark ~= nil and stackeddamage > enemyLife then
 		CastSpell(_R)
 	end
+	--if deathMark == nil and not ValidTarget(unit, GetCastRange(myHero,_E)) and Ready(_R) then
+		--CastSpell(_R)
+	--end
 end
 
 OnUpdateBuff (function(unit, buff)
@@ -135,10 +200,13 @@ OnRemoveBuff (function(unit, buff)
 	if buff.Name == "zedrdeathmark" then
 		print("Now we can go back!")
 		deathMark = nil
+		savestacked = stackeddamage
+		stackeddamage = 0
     end
 end)
 
 function gapClose(unit)
+if deathMark ~= true then
 	if Zed.Combo.W:Value() then
 		if Ready(_W) and ValidTarget(unit, GetRange(myHero)+GetCastRange(myHero,_W)) and not IsInDistance(unit, GetCastRange(myHero,_E)+GetRange(myHero)) then
 			local GaWPred = GetPredictionForPlayer(myHeroPos(),unit,GetMoveSpeed(unit),300,300,GetCastRange(myHero,_W)+GetRange(myHero),250,false,false)
@@ -148,68 +216,67 @@ function gapClose(unit)
 		end
 	end
 end
-
-OnCreateObj(function(Object)
-	if GetObjectBaseName(Object) == "Shadow" then
-		WPos = Object
-		RPos = Object
-	end
-	--PrintChat(string.format("<font color='#00ff00'>CreatedObject= %s</font>",GetObjectBaseName(Object)))
-end)
-OnDeleteObj(function(Object,myHero)
-	if GetObjectBaseName(Object) == "Shadow" then
-		WPos = nil
-		RPos = nil
-	end
-end)
-
-function DrawDamage(unit)
-	if ValidTarget(unit, 1400) then
-		local Qdmg = getdmg("Q",unit,myHero,1)
-		if Ready(_Q) then
-			DrawDmgOverHpBar(unit,GetCurrentHP(unit),Qdmg,0,0xff00ff00)
-		end
-		local Edmg = getdmg("E",unit,myHero,1)
-		if Ready(_E) then
-			DrawDmgOverHpBar(unit,GetCurrentHP(unit),Edmg,0,0xff00ff00)
-		end
-		local Rdmg = getdmg("R",unit,myHero,3)
-		if Ready(_R) then
-			DrawDmgOverHpBar(unit,GetCurrentHP(unit),Rdmg,0,0xff00ff00)
-		end
-	end
 end
+-- Credits to Deftsu
+OnCreateObj(function(Object)
+  if GetObjectBaseName(Object) == "Shadow" then
+    table.insert(objects, Object)
+  end
+end)
 
-OnDraw(function(myHero)
+OnDeleteObj(function(Object)
+  if GetObjectBaseName(Object) == "Shadow" then
+    for i, obj in pairs(objects) do
+      if obj == Object then
+        table.remove(objects, i)
+      end
+	end
+  end
+end)
+
+OnDraw(function(myHero) -- example
 	if CanUseSpell(myHero, _Q) == READY and Zed.Draw.DrawQ:Value() then DrawCircle(myHeroPos().x,myHeroPos().y,myHeroPos().z,GetCastRange(myHero,_Q),3,100,GoS.Red) end
 	if CanUseSpell(myHero, _E) == READY and Zed.Draw.DrawE:Value() then DrawCircle(myHeroPos().x,myHeroPos().y,myHeroPos().z,GetCastRange(myHero,_E),3,100,GoS.Green) end
 	if Zed.Drawings.DrawS:Value() then
-		if RPos ~= nil and CanUseSpell(myHero,_Q) == READY and CanUseSpell(myHero,_E) == READY then
-			DrawCircle(GetOrigin(RPos), GetCastRange(myHero,_Q),1,50,GoS.Blue)
-			DrawCircle(GetOrigin(RPos), GetCastRange(myHero,_E),1,50,GoS.Blue)
-			DrawCircle(GetOrigin(RPos), 50,3,50,GoS.White)
-		elseif RPos ~= nil and CanUseSpell(myHero,_Q) == READY then
-			DrawCircle(GetOrigin(RPos), GetCastRange(myHero,_Q),1,50,GoS.Blue)
-			DrawCircle(GetOrigin(RPos), 50,3,50,GoS.White)
-		elseif RPos ~= nil and CanUseSpell(myHero,_E) == READY then
-			DrawCircle(GetOrigin(RPos), GetCastRange(myHero,_E),1,50,GoS.Blue)
-			DrawCircle(GetOrigin(RPos), 50,3,50,GoS.White)
-		elseif RPos ~= nil then
-			DrawCircle(GetOrigin(RPos), 50,3,50,GoS.White)
+		for i, obj in pairs(objects) do -- objects table
+			DrawCircle(GetOrigin(obj), GetCastRange(myHero,_Q),1,50,GoS.Blue)
+			DrawCircle(GetOrigin(obj), GetCastRange(myHero,_E),1,50,GoS.Blue)
+			DrawCircle(GetOrigin(obj), 50,3,50,GoS.White)
 		end
-		-- W Drawings
-		if WPos ~= nil and CanUseSpell(myHero,_Q) == READY and CanUseSpell(myHero,_E) == READY then
-			DrawCircle(GetOrigin(WPos), GetCastRange(myHero,_Q),1,50,GoS.Blue)
-			DrawCircle(GetOrigin(WPos), GetCastRange(myHero,_E),1,50,GoS.Blue)
-			DrawCircle(GetOrigin(WPos), 50,3,50,GoS.White)
-		elseif WPos ~= nil and CanUseSpell(myHero,_Q) == READY then
-			DrawCircle(GetOrigin(WPos), GetCastRange(myHero,_Q),1,50,GoS.Blue)
-			DrawCircle(GetOrigin(WPos), 50,3,50,GoS.White)
-		elseif WPos ~= nil and CanUseSpell(myHero,_E) == READY then
-			DrawCircle(GetOrigin(WPos), GetCastRange(myHero,_E),1,50,GoS.Blue)
-			DrawCircle(GetOrigin(WPos), 50,3,50,GoS.White)
-		elseif WPos ~= nil then
-			DrawCircle(GetOrigin(WPos), 50,3,50,GoS.White)
+	end
+	-- Credits end here
+local unit = GetCurrentTarget()
+local Qdmg = getdmg("Q",unit,myHero,1,GetCastLevel(myHero,_Q))
+local Edmg = getdmg("E",unit,myHero,1,GetCastLevel(myHero,_E))
+local Rdmg = getdmg("R",unit,myHero,1,GetCastLevel(myHero,_R))
+local physical = GetBaseDamage(myHero) + GetBonusDmg(myHero)
+local magical = GetBonusAP(myHero)
+local TotalDamage = physical + magical * .1
+	if ValidTarget(unit, 1400) then
+		--[[local Qdmg = getdmg("Q",unit,myHero,1,GetCastLevel(myHero,_Q))
+		if Ready(_Q) then
+			DrawDmgOverHpBar(unit,GetCurrentHP(unit),Qdmg,0,0xff00ff00)
+		end
+		local Edmg = getdmg("E",unit,myHero,1,GetCastLevel(myHero,_E))
+		if Ready(_E) then
+			DrawDmgOverHpBar(unit,GetCurrentHP(unit),Edmg,0,0xff00ff00)
+		end]]
+		local Rdmg = getdmg("R",unit,myHero,1,GetCastLevel(myHero,_R))
+		if Ready(_R) then
+			DrawDmgOverHpBar(unit,GetCurrentHP(unit),savestacked,0,0xff00ff00)
+		end
+		if deathMark ~= nil then
+		   DrawDmgOverHpBar(unit,GetCurrentHP(unit),stackeddamage,0,0xff00ff00)
+		end
+	end
+end)
+--More Credits to Deftsu
+OnDamage(function(unit, target, damage)
+local enemyHero = GetCurrentTarget()
+	if unit == myHero and target == enemyHero then
+		if deathMark ~= nil then
+		 stackeddamage = stackeddamage + damage
+		 --PrintChat(string.format("<font color='#ff0000'>OnDamage:</font> Sender: [%s] Receiver: [%s] Damage: [%f]",GetObjectName(unit),GetObjectName(target),damage));
 		end
 	end
 end)
